db270a26a13dc8191ccc2d3f01c6e32987a77b12
Seele <48525679+Seele0oO@users.noreply.github.com>
2023-11-05 15:30:57 +0800

    add targetType tag

--- rules/nitter.js
+++ rules/nitter.js
@@ -5,6 +5,7 @@
             title: "Tweets",
             docs: "https://github.com/zedeus/nitter",
             source: "/:id",
+            targetType: "url",
             target: (params) => {
                 if (params.id !== 'home' && params.id !== 'explore' && params.id !== 'notifications' && params.id !== 'messages' && params.id !== 'explore' && params.id !== 'search') {
                     return 'https://twiiit.com/:id/rss';
@@ -15,6 +16,7 @@
             title: "Tweets & Replies",
             docs: "https://github.com/zedeus/nitter",
             source: "/:id",
+            targetType: "url",
             target: (params) => {
                 if (!['home', 'explore', 'notifications', 'messages', 'explore', 'search'].includes(params.id)) {
                     return 'https://twiiit.com/:id/with_replies/rss';
@@ -25,12 +27,13 @@
             title: "Media",
             docs: "https://github.com/zedeus/nitter",
             source: "/:id",
+            targetType: "url",
             target: (params) => {
                 if (params.id !== 'home' && params.id !== 'explore' && params.id !== 'notifications' && params.id !== 'messages' && params.id !== 'explore' && params.id !== 'search') {
                     return 'https://twiiit.com/:id/media/rss';
                 }
             }
         }
-    ]
+        ]
     },
 })
